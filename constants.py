2b012a099bb17cd2c41e65bc4acb95ac39f011d2
eric <e.salberg213@gmail.com>
2021-02-09 00:20:52 -0500

    added player two

--- constants.py
+++ constants.py
@@ -24,6 +24,15 @@ BLACK = (0, 0 , 0)
 # RGB value for Blue
 BLUE = (0, 0, 255)
 
+# RGB value for Grey
+GREY = (140, 140, 140)
+
+# RGB value off-white
+OFF_WHITE = (220, 220, 220)
+
+# RGB value off-black
+OFF_BLACK = (54, 54, 54)
+
 # width of screen 
 SCREEN_WIDTH = 1143
 
@@ -46,10 +55,13 @@ MIN_MOVE = 0
 MAX_MOVE = 4
 
 # location of player score
-PLAYER_SCORE_CENTER = (640, 75)
+# PLAYER_SCORE_CENTER = (640, 75)
 
 # player one sequence of squares
-PLAYER_SQUARES_SEQUENCE = [(500, 75), (360, 75), (215, 75), (75, 75), (75, 225), (215, 225), (360, 225), (500, 225), (642, 225), (785, 225), (925, 225), (1070, 225), (1070, 75), (925, 75), (780, 75)]
+PLAYER_ONE_SQUARES_SEQUENCE = [(500, 75), (360, 75), (215, 75), (75, 75), (75, 225), (215, 225), (360, 225), (500, 225), (642, 225), (785, 225), (925, 225), (1070, 225), (1070, 75), (925, 75), (780, 75)]
+
+# player two sequence of squares
+PLAYER_TWO_SQUARES_SEQUENCE = [(500, 365), (360, 365), (215, 365), (75, 365), (75, 225), (215, 225), (360, 225), (500, 225), (642, 225), (785, 225), (925, 225), (1070, 225), (1070, 365), (925, 365), (780, 365)]
 
 # squares only available to player 1
 PLAYER_ONE_SQUARES = [(75, 75), (215, 75), (360, 75), (500, 75), (640, 75), (780, 75), (925, 75), (1070, 75)]
@@ -62,9 +74,9 @@ PLAYER_TWO_SQUARES = [(75, 365), (215, 365), (360, 365), (500, 365), (640, 365),
 
 BOARD_SQUARES_LOCATIONS = [PLAYER_ONE_SQUARES, SHARED_SQUARES, PLAYER_TWO_SQUARES]
 
-# Player 1 key mappings
-P1_KEY_MAPPINGS = {'left' : K_LEFT, 'right' : K_RIGHT, 'up' : K_UP, 'down' : K_DOWN}
+# Player 1 mappings
+P1_MAPPINGS = {'color' : WHITE, 'cursor_color' : OFF_BLACK, 'move_seq' : PLAYER_ONE_SQUARES_SEQUENCE, 'score_center' : (640, 75), 'keys' : {'left' : K_LEFT, 'right' : K_RIGHT, 'up' : K_UP, 'down' : K_DOWN}}
 
 # Player 2 key mappings
-P2_KEY_MAPPINGS ={'left' : K_a, 'right' : K_d, 'up' : K_w, 'down' : K_s}
+P2_MAPPINGS = {'color' : BLACK, 'cursor_color' : OFF_WHITE, 'move_seq' : PLAYER_TWO_SQUARES_SEQUENCE, 'score_center' : (780, 75), 'keys' : {'left' : K_a, 'right' : K_d, 'up' : K_w, 'down' : K_s}}
 
